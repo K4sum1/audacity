2dbe926457d02ef474e17f320a0f8d332e2f35a2
Carlo Bramini <30959007+carlo-bramini@users.noreply.github.com>
2020-05-25 08:37:55 +0200

    [WIN32] Correctly read default value of wxRegKey entry. (#456)
    Fix this build error with GCC:
    
    ../../audacity/src/AudacityApp.cpp:2347:62: error: call of overloaded 'wxString(wxRegKey&)' is ambiguous.

--- src/AudacityApp.cpp
+++ src/AudacityApp.cpp
@@ -2365,7 +2365,7 @@ void AudacityApp::AssociateFileTypes()
                associateFileTypes.SetName(root_key + wxT("Audacity.Project\\shell\\open\\command"));
                wxString tmpRegAudPath;
                if(associateFileTypes.Exists()) {
-                  tmpRegAudPath = wxString(associateFileTypes).Lower();
+                  tmpRegAudPath = associateFileTypes.QueryDefaultValue().Lower();
                }
                if (!associateFileTypes.Exists() ||
                      (tmpRegAudPath.Find(wxT("audacity.exe")) >= 0)) {
